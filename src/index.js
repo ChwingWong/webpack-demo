ceab09fd43acb3fff02a4141c0cd5c8cac379f9e
Chwing <chwingwong@outlook.com>
2018-01-17 22:08:36 +0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+
 
 function component() {
     var element = document.createElement('div');
@@ -9,12 +7,7 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
-// Add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
 
-    console.log(Data);
 
     return element;
 }
