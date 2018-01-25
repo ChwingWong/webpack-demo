50cfadb31fe0752eb1ef08f5e47af6cb06c31030
Chwing <chwingwong@outlook.com>
2018-01-25 18:40:25 +0800

    Shimming
    需重来

--- webpack.common.js
+++ webpack.common.js
@@ -5,15 +5,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
+        polyfills: './src/polyfills.js',
         index: './src/index.js',
-        vendor: [
-            'lodash'
-        ]
+
+        // vendor: [
+        //     'lodash'
+        // ]
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        // new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Caching'
+        }),
+        new webpack.ProvidePlugin({
+            // _: 'lodash'
+            join: ['lodash', 'join']
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor'
@@ -26,5 +32,13 @@ module.exports = {
         filename: '[name].[chunkhash].bundle.js',
         chunkFilename: '[name].[chunkhash].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: require.resolve('./src/index.js'),
+                use: 'imports-loader?this=>window'
+            }
+        ]
+    },
 };
